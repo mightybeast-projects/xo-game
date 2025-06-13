acc4ff15ac1edf9ef573d577f54a9b0970b07186
Єфремов Євгеній Олексійович <Yevgenij.Yefremov@feofaniya.lan>
2025-06-13 12:39:11 +0300

    -refactor- : Tests refactoring.

--- C++/xo/src/core/xo/win-conditions-tests.cpp
+++ C++/xo/src/core/xo/win-conditions-tests.cpp
@@ -4,93 +4,93 @@
 using namespace xo;
 using namespace testing;
 
-struct XOWinConditions : public Test
+struct XOGame : public Test
 {
     XO xo = XO(3);
 };
 
-TEST_F(XOWinConditions, Winner_In_Newly_Created_XO_Should_Not_Be_Defined)
+TEST_F(XOGame, Winner_Should_Not_Be_Defined_On_Initialization)
 {
     EXPECT_EQ(xo.winner(), std::nullopt);
 }
 
-TEST_F(XOWinConditions, Should_Have_Winner_With_Diagonal_Win_Condition_1)
-{
-    xo.place(0, 0, X);
-    xo.place(1, 1, X);
-    xo.place(2, 2, X);
-
-    EXPECT_EQ(xo.winner().has_value(), true);
-}
-
-TEST_F(XOWinConditions, Should_Have_Winner_With_Diagonal_Win_Condition_2)
-{
-    xo.place(0, 2, X);
-    xo.place(1, 1, X);
-    xo.place(2, 0, X);
-
-    EXPECT_EQ(xo.winner().has_value(), true);
-}
-
-TEST_F(XOWinConditions, Should_Have_Winner_With_Vertical_Win_Condition_1)
-{
-    xo.place(0, 0, X);
-    xo.place(0, 1, X);
-    xo.place(0, 2, X);
-
-    EXPECT_EQ(xo.winner().has_value(), true);
-}
-
-TEST_F(XOWinConditions, Should_Have_Winner_With_Vertical_Win_Condition_2)
-{
-    xo.place(1, 0, X);
-    xo.place(1, 1, X);
-    xo.place(1, 2, X);
-
-    EXPECT_EQ(xo.winner().has_value(), true);
-}
-
-TEST_F(XOWinConditions, Should_Have_Winner_With_Vertical_Win_Condition_3)
-{
-    xo.place(2, 0, X);
-    xo.place(2, 1, X);
-    xo.place(2, 2, X);
-
-    EXPECT_EQ(xo.winner().has_value(), true);
-}
-
-TEST_F(XOWinConditions, Should_Have_Winner_With_Horizontal_Win_Condition_1)
-{
-    xo.place(0, 0, X);
-    xo.place(1, 0, X);
-    xo.place(2, 0, X);
-
-    EXPECT_EQ(xo.winner().has_value(), true);
-}
-
-TEST_F(XOWinConditions, Should_Have_Winner_With_Horizontal_Win_Condition_2)
-{
-    xo.place(0, 1, X);
-    xo.place(1, 1, X);
-    xo.place(2, 1, X);
-
-    EXPECT_EQ(xo.winner().has_value(), true);
-}
-
-TEST_F(XOWinConditions, Should_Have_Winner_With_Horizontal_Win_Condition_3)
-{
-    xo.place(0, 2, X);
-    xo.place(1, 2, X);
-    xo.place(2, 2, X);
-
-    EXPECT_EQ(xo.winner().has_value(), true);
-}
-
-TEST_F(XOWinConditions, Should_Get_Winner)
+TEST_F(XOGame, Should_Have_Winner_If_Winning_Condition_Is_Met)
 {
     xo.place(0, 2, O);
     xo.place(1, 2, O);
     xo.place(2, 2, O);
 
     EXPECT_EQ(xo.winner(), O);
+}
+
+TEST_F(XOGame, Should_Have_Winner_When_Diagonal_Win_Condition_Is_Met_1)
+{
+    xo.place(0, 0, X);
+    xo.place(1, 1, X);
+    xo.place(2, 2, X);
+
+    EXPECT_EQ(xo.winner().has_value(), true);
+}
+
+TEST_F(XOGame, Should_Have_Winner_When_Diagonal_Win_Condition_Is_Met_2)
+{
+    xo.place(0, 2, X);
+    xo.place(1, 1, X);
+    xo.place(2, 0, X);
+
+    EXPECT_EQ(xo.winner().has_value(), true);
+}
+
+TEST_F(XOGame, Should_Have_Winner_When_Vertical_Win_Condition_Is_Met_1)
+{
+    xo.place(0, 0, X);
+    xo.place(0, 1, X);
+    xo.place(0, 2, X);
+
+    EXPECT_EQ(xo.winner().has_value(), true);
+}
+
+TEST_F(XOGame, Should_Have_Winner_When_Vertical_Win_Condition_Is_Met_2)
+{
+    xo.place(1, 0, X);
+    xo.place(1, 1, X);
+    xo.place(1, 2, X);
+
+    EXPECT_EQ(xo.winner().has_value(), true);
+}
+
+TEST_F(XOGame, Should_Have_Winner_When_Vertical_Win_Condition_Is_Met_3)
+{
+    xo.place(2, 0, X);
+    xo.place(2, 1, X);
+    xo.place(2, 2, X);
+
+    EXPECT_EQ(xo.winner().has_value(), true);
+}
+
+TEST_F(XOGame, Should_Have_Winner_When_Horizontal_Win_Condition_Is_Met_1)
+{
+    xo.place(0, 0, X);
+    xo.place(1, 0, X);
+    xo.place(2, 0, X);
+
+    EXPECT_EQ(xo.winner().has_value(), true);
+}
+
+TEST_F(XOGame, Should_Have_Winner_When_Horizontal_Win_Condition_Is_Met_2)
+{
+    xo.place(0, 1, X);
+    xo.place(1, 1, X);
+    xo.place(2, 1, X);
+
+    EXPECT_EQ(xo.winner().has_value(), true);
+}
+
+TEST_F(XOGame, Should_Have_Winner_When_Horizontal_Win_Condition_Is_Met_3)
+{
+    xo.place(0, 2, X);
+    xo.place(1, 2, X);
+    xo.place(2, 2, X);
+
+    EXPECT_EQ(xo.winner().has_value(), true);
 }
--- C++/xo/src/core/xo/xo-tests.cpp
+++ C++/xo/src/core/xo/xo-tests.cpp
@@ -5,24 +5,24 @@ using namespace xo;
 using namespace testing;
 using namespace std;
 
-struct XOGeneral : public Test
+struct XOGame : public Test
 {
     XO xo = XO(3);
 };
 
-TEST_F(XOGeneral, Should_Have_Size_Specified_On_Initialization)
+TEST_F(XOGame, Should_Have_Size_Specified_On_Initialization)
 {
     EXPECT_EQ(xo.size(), 3);
 }
 
-TEST_F(XOGeneral, Newly_Created_XO_Should_Have_Empty_Cells)
+TEST_F(XOGame, Should_Have_Empty_Cells_On_Initialization)
 {
     for (auto i = 0; i < xo.size(); i++)
         for (auto j = 0; j < xo.size(); j++)
             EXPECT_EQ(xo.state()[i][j], nullopt);
 }
 
-TEST_F(XOGeneral, Should_Place_Value)
+TEST_F(XOGame, Should_Place_Value)
 {
     xo.place(1, 2, X);
     xo.place(2, 2, O);
@@ -31,7 +31,7 @@ TEST_F(XOGeneral, Should_Place_Value)
     EXPECT_EQ(xo.state()[2][2], O);
 }
 
-TEST_F(XOGeneral, Should_Not_Place_Value_On_Already_Occupied_Cell)
+TEST_F(XOGame, Should_Not_Place_Value_On_Already_Occupied_Cell)
 {
     xo.place(1, 2, X);
     xo.place(1, 2, O);
@@ -39,7 +39,7 @@ TEST_F(XOGeneral, Should_Not_Place_Value_On_Already_Occupied_Cell)
     EXPECT_EQ(xo.state()[1][2], X);
 }
 
-TEST_F(XOGeneral, Should_Not_Place_Value_If_Index_Is_Out_Of_Bounds)
+TEST_F(XOGame, Should_Not_Place_Value_If_Index_Is_Out_Of_Bounds)
 {
     xo.place(0, 3, X);
     xo.place(-1, 1, X);
@@ -49,7 +49,7 @@ TEST_F(XOGeneral, Should_Not_Place_Value_If_Index_Is_Out_Of_Bounds)
             EXPECT_EQ(xo.state()[i][j], nullopt);
 }
 
-TEST_F(XOGeneral, Should_Not_Place_Value_If_Player_Already_Won)
+TEST_F(XOGame, Should_Not_Place_Value_If_It_Has_A_Winner)
 {
     xo.place(0, 0, X);
     xo.place(1, 1, X);
@@ -60,14 +60,14 @@ TEST_F(XOGeneral, Should_Not_Place_Value_If_Player_Already_Won)
     EXPECT_EQ(xo.state()[0][1], nullopt);
 }
 
-TEST_F(XOGeneral, Should_Place_Next_Value_Starting_With_X)
+TEST_F(XOGame, Should_Place_Next_Value_Starting_With_X)
 {
     xo.placeNext(0, 2);
 
     EXPECT_EQ(xo.state()[0][2], X);
 }
 
-TEST_F(XOGeneral, Should_Switch_Value_After_Every_Next_Placement)
+TEST_F(XOGame, Should_Switch_Value_After_Every_Next_Placement)
 {
     xo.placeNext(0, 1);
     xo.placeNext(1, 1);
