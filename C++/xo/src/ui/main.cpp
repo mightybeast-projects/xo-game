4415b711ca604d3f4213a3260e3f555399506817
Єфремов Євгеній Олексійович <Yevgenij.Yefremov@feofaniya.lan>
2025-06-16 16:35:43 +0300

    -chore- : Fcking around with example.

--- C++/xo/src/ui/main.cpp
+++ C++/xo/src/ui/main.cpp
@@ -2,25 +2,40 @@
 #include "raylib.h"
 #include "raygui.h"
 
+bool showMessageBox = false;
+Color bgColor = RED;
+
 int main()
 {
-    InitWindow(400, 200, "raygui - controls test suite");
+    InitWindow(400, 400, "XOGame");
     SetTargetFPS(60);
 
-    bool showMessageBox = false;
+    GuiButton((Rectangle){100, 200, 120, 30}, "#191#Show Message");
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
+        ClearBackground(bgColor);
+
+        DrawLine(0, 0, 300, 500, GREEN);
 
         if (GuiButton((Rectangle){24, 24, 120, 30}, "#191#Show Message"))
             showMessageBox = true;
 
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+            bgColor = GREEN;
+
+        if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
+            bgColor = YELLOW;
+
+        if (IsMouseButtonPressed(MOUSE_BUTTON_MIDDLE))
+            bgColor = RED;
+
         if (showMessageBox)
         {
-            int result = GuiMessageBox((Rectangle){85, 70, 250, 100},
-                                       "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
+            int result = GuiMessageBox(
+                (Rectangle){85, 70, 250, 100},
+                "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
 
             if (result >= 0)
                 showMessageBox = false;
